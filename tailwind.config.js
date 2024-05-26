b154d1437dc3d70029e0be88c48e078ba66b3780
khalilMaat <maatoug620@outlook.fr>
2024-05-26 19:13:05 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+  "./src/**/*.{html,ts}",
+  ],
+  theme: {
+  extend: {
+    colors: {
+      'my-blue': '#007bff',
+      },
+      
+  },
+  },
+  plugins: [],
+  variants: {},
+
+  }
